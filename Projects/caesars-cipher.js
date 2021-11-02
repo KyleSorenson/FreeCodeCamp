c09a96635545e2616a1ede5a30e2997ac85f97b1
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-11-01 22:55:52 -0600

    Delete Projects directory

--- Projects/caesars-cipher.js
+++
@@ -1,9 +0,0 @@
-function rot13(str) {
-  const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return str.replace(
-    /[A-Z]/g,
-    i => ALPHABET[
-      (ALPHABET.match(i).index + 13) % 26
-    ]
-  );
-}
