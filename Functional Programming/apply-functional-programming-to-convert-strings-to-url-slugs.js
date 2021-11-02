ca28c622c108291de9542001154f4081056a32cf
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-11-01 22:55:31 -0600

    Delete Functional Programming directory

--- Functional Programming/apply-functional-programming-to-convert-strings-to-url-slugs.js
+++
@@ -1,7 +0,0 @@
-// Only change code below this line
-function urlSlug(title) {
-  return title.toLowerCase().match(/\w+/g).join('-');
-}
-// Only change code above this line
-
-console.log(urlSlug(" Winter Is  Coming"))
