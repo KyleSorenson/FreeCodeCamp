f051be4b643e015ad89a898243d7338c9387c976
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-11-04 18:46:18 -0600

    Update caesars-cipher.js

--- JavaScript Algorithms and Data Structures/Projects/caesars-cipher.js
+++ JavaScript Algorithms and Data Structures/Projects/caesars-cipher.js
@@ -1,7 +1,15 @@
 function rot13(str) {
   const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
   return str.replace(
+    
+    // regex that returns each letter in str (and excludes spaces and punctuation)
     /[A-Z]/g,
+    
+    // for every letter in str,
+    // the position in the alphabet is found
+    // and shifted 13 places
+    // (looping from the end of the alphabet back to the beginning)
     i => ALPHABET[
       (ALPHABET.match(i).index + 13) % 26
     ]
