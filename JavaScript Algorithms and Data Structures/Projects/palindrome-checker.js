292e88a161843597cb5f0b7f84c5d03d6dc379d1
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-11-05 15:26:51 -0600

    Update palindrome-checker.js

--- JavaScript Algorithms and Data Structures/Projects/palindrome-checker.js
+++ JavaScript Algorithms and Data Structures/Projects/palindrome-checker.js
@@ -1,4 +1,14 @@
 function palindrome(str) {
-  let strArray = str.toLowerCase().match(/[a-z0-9]/g);
+  
+  // Makes str lowercase so that letters will all
+  // be compared in the same case.
+  let strArray = str.toLowerCase()
+
+  // Creates an array of each individual letter
+  // or number. Excludes spaces and other characters
+  .match(/[a-z0-9]/g);
+  
+  // Tests to see if the reversed array (backwards-
+  // to-forwards) is identical to the forwards-to-backwards array
   return (strArray.join('') === strArray.reverse().join('')) ? true : false
 }
