07e118f2106bf8756158ca1cb3195694f7ce59ec
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-11-08 21:11:47 -0700

    Delete binary-agents.js

--- JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/binary-agents.js
+++
@@ -1,7 +0,0 @@
-function binaryAgent(str) {
-  let binaryArray = str.split(' ');
-  let charArray = binaryArray.map(i => 
-    String.fromCharCode(String(parseInt(i, 2)))
-  );
-  return charArray.join('');
-}
