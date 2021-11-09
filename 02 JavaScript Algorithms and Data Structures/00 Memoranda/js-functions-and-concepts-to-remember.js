a9c6f550b619aa3e57691b8669f13e711fc85b59
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-11-08 21:48:59 -0700

    Create js-functions-and-concepts-to-remember.js

---
+++ 02 JavaScript Algorithms and Data Structures/00 Memoranda/js-functions-and-concepts-to-remember.js
@@ -0,0 +1,47 @@
+// Exporting and Importing Functions
+
+// Nested Ternary Operators
+
+// Template Literals
+
+// Destructuring Assignment
+
+// Arrow Functions
+
+// Concise Declarative Functions
+
+// Callback Functions
+
+	// Example to better understand:
+	function findLongestWordLength(s) {
+	  return s.split(' ')
+	    .reduce(function(longest, word) {
+	      return Math.max(longest, word.length)
+	    }, 0);
+	}
+
+// Getter and Setter Functions
+
+// Class Syntax and Constructor Functions
+
+// Immediately Invoked Function Expressions
+
+// Curried Functions
+
+// Function Methods: call(), apply(), bind()
+
+// Partial Applications
+
+// reduce()
+
+	// Free Code Camp Lesson: Use the Reduce Method to Analyze Data
+
+// Concise Object Literal Declaration
+
+// Closures
+
+// Function Factories
+
+// Rest Parameter and Spread Operator
+
+// Promises, "then" Fulfillment, "catch" Rejection
